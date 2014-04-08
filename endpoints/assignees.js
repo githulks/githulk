f6a50345fc553b5c3ceb5b82908b3c95f2bec05a
3rd-Eden <info@3rd-Eden.com>
2014-04-08 21:28:44 +0200

    [minor] Added assignee api

---
+++ endpoints/assignees.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ *
+ * @param {Mana} api The actual API instance.
+ * @api private
+ */
+function Assignees(api) {
+  this.send = api.send.bind(api);
+  this.qs = api.querystringify;
+  this.api = api;
+}
+
+/**
+ * Get a list of users that can get assigned.
+ *
+ * @param {String} project User/repo combination.
+ * @param {Options} options Optional options.
+ * @param {Function} fn The callback.
+ * @api public
+ */
+Assignees.prototype.get = function get(args) {
+  args = this.api.args(arguments);
+
+  var project = this.api.project(args.str)
+    , options = args.options || {};
+
+  return this.send(
+    ['repos', project.user, project.repo, 'assignees'],
+    options,
+    args.fn
+  );
+};
+
+/**
+ * Check if a given assignee is allowed for the repository.
+ *
+ * @param {String} project User/repo combination.
+ * @param {Options} options Optional options.
+ * @param {Function} fn The callback.
+ * @api public
+ */
+Assignees.prototype.assignee = function assignee(args) {
+  args = this.api.args(arguments);
+
+  var project = this.api.project(args.str)
+    , options = args.options || {};
+
+  return this.send(
+    ['repos', project.user, project.repo, options.assignee],
+    options,
+    args.fn
+  );
+};
+
+//
+// Expose the assignees API.
+//
+module.exports = Assignees;
