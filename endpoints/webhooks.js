2baf667f32bffbee1b95005d2ebc53b60ed15a19
Jarrett Cruger <jcrugzz@gmail.com>
2015-10-28 23:35:57 -0700

    [api] add webhooks api for creation

---
+++ endpoints/webhooks.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Webhooks API endpoint
+ *
+ * @param {mana} api The actual API instance
+ * @api private
+ */
+function Webhook(api) {
+  this.send = api.send.bind(api);
+  this.options = api.options;
+  this.api = api;
+}
+
+Webhook.create = [
+  'name', // Name of a valid webhook service
+  'config', // config, object with k, v for the service
+  'events', // array of events that we hook into
+  'active'
+];
+
+/**
+ * Create a new webhook for the project
+ *
+ */
+Webhook.prototype.create = function (args) {
+  args = this.api.args(arguments);
+  args.options = this.options(args.options, Webhook.create);
+
+  var project = this.api.project(args.str);
+
+  return this.send(
+    ['repos', project.user, project.repo, 'hooks'],
+    args.options,
+    args.fn
+  );
+};
