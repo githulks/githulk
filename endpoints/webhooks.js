ec24101bb68bcce5b141e288720b4bef0aae67a3
Jarrett Cruger <jcrugzz@gmail.com>
2015-11-03 16:19:37 -0800

    [fix] finish webhook implementation to return expected responses

--- endpoints/webhooks.js
+++ endpoints/webhooks.js
@@ -13,6 +13,19 @@ function Webhook(api) {
 }
 
 /**
+ * Unwrap a result into an object when necessary
+ */
+function unwrapper(fn) {
+  return function unwrap(err, results) {
+    if (err) { return fn(err); }
+    return fn(undefined, results
+        && results.length <= 1
+          ? results[0]
+          : results);
+  };
+}
+
+/**
  * Properties we need in the body of the request for create
  */
 Webhook.create = [
@@ -24,18 +37,75 @@ Webhook.create = [
 
 /**
  * Create a new webhook for the project
- *
  */
 Webhook.prototype.create = function (args) {
   args = this.api.args(arguments);
-  args.options = this.options(args.options, Webhook.create);
+  args.options = this.options(this.api.merge(args.options, { method: 'POST' }), Webhook.create);
 
   var project = this.api.project(args.str);
 
   return this.send(
     ['repos', project.user, project.repo, 'hooks'],
     args.options,
-    args.fn
+    unwrapper(args.fn)
+  );
+};
+
+/**
+ * Get a webhooks by id
+ */
+Webhook.prototype.get = function (args) {
+  args = this.api.args(arguments);
+
+  var project = this.api.project(args.str);
+  args.options = this.options(args.options);
+  var id = args.options.id;
+
+  if (!id) {
+    return args.fn(new Error('id is required parameter'));
+  }
+
+  return this.send(
+    ['repos', project.user, project.repo, 'hooks', id],
+    args.options,
+    unwrapper(args.fn)
+  );
+};
+
+/**
+ * Delete a webhook for the given project and id
+ */
+Webhook.prototype.delete = function (args) {
+  args = this.api.args(arguments);
+
+  var project = this.api.project(args.str);
+  args.options = this.options(this.api.merge(args.options, { method: 'DELETE' }), []);
+  var id = args.options.id;
+
+  if (!id) {
+    return args.fn(new Error('id is required parameter'));
+  }
+
+  return this.send(
+    ['repos', project.user, project.repo, 'hooks', id],
+    args.options,
+    unwrapper(args.fn)
+  );
+};
+
+/**
+ * List the webhooks for a given repo
+ */
+Webhook.prototype.list = function (args) {
+  args = this.api.args(arguments);
+  args.options = this.options(args.options);
+
+  var project = this.api.project(args.str);
+
+  return this.send(
+    ['repos', project.user, project.repo, 'hooks'],
+    args.options,
+    unwrapper(args.fn)
   );
 };
 
